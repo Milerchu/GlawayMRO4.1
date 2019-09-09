2e4c7cc01e5774aa30f4044eae69bd73369801fa
Millerchu <zhuhao0514@hotmail.com>
2019-09-09 21:22:54 +0800

    修改检修工单选择客户料库房处理

--- src/com/glaway/sddq/service/failureord/data/FldIsCustItem.java
+++ src/com/glaway/sddq/service/failureord/data/FldIsCustItem.java
@@ -6,6 +6,7 @@ import com.glaway.mro.jpo.JpoField;
 import com.glaway.mro.system.MroServer;
 import com.glaway.mro.util.GWConstant;
 import com.glaway.sddq.tools.ItemUtil;
+import com.glaway.sddq.tools.SddqConstant;
 
 /**
  * 
@@ -28,42 +29,45 @@ public class FldIsCustItem extends JpoField {
 
 		// 当前输入值
 		int input = this.getInputMroType().asInt();
-		if ("FAILUREORD".equalsIgnoreCase(getJpo().getAppName())) {
-			if (isValueChanged()) {
-				// 清空上车件数据
-				String[] upAssets = new String[] { "NEWSQN", "NEWASSETNUM",
-						"NEWITEMNUM", "NEWLOC", "NEWBINNUM", "NEWLOTNUM" };
-				for (String upAsset : upAssets) {
+		// 检修or服务
+		String jxorfw = "";
+		if (SddqConstant.SXC_GZ.equalsIgnoreCase(getJpo().getString("tasktype"))) {
+			jxorfw = ItemUtil.JXORFW_FW ;
+		}else if(SddqConstant.SXC_OH.equalsIgnoreCase(getJpo().getString("tasktype"))){
+			jxorfw = ItemUtil.JXORFW_JX ;
+		}
 
-					getJpo().setValueNull(upAsset);
+		if (isValueChanged()) {
+			// 清空上车件数据
+			String[] upAssets = new String[] { "NEWSQN", "NEWASSETNUM",
+					"NEWITEMNUM", "NEWLOC", "NEWBINNUM", "NEWLOTNUM" };
+			for (String upAsset : upAssets) {
 
-				}
+				getJpo().setValueNull(upAsset);
+
 			}
-			// 选择是，则库房过滤改为其他待处理库
-			if (input == 1) {
+		}
+		// 选择是，则库房过滤改为其他待处理库
+		if (input == 1) {
 
-				String station = getJpo().getParent().getParent()
-						.getString("WHICHSTATION");
-				// 其它待处理库
-				String where = "erploc='" + ItemUtil.ERPLOC_QTDCL
-						+ "' and STOREROOMLEVEL in('"
-						+ ItemUtil.STOREROOMLEVEL_XCK + "','"
-						+ ItemUtil.STOREROOMLEVEL_XCZDK
-						+ "') and locationtype='" + ItemUtil.LOCATIONTYPE_CG
-						+ "' and jxorfw='" + ItemUtil.JXORFW_FW
-						+ "' and locsite ='" + station + "'";
+			String station = getJpo().getParent().getParent().getString("WHICHSTATION");
+			// 其它待处理库
+			String where = "erploc='" + ItemUtil.ERPLOC_QTDCL
+					+ "' and STOREROOMLEVEL in('"
+					+ ItemUtil.STOREROOMLEVEL_XCK + "','"
+					+ ItemUtil.STOREROOMLEVEL_XCZDK
+					+ "') and locationtype='" + ItemUtil.LOCATIONTYPE_CG
+					+ "' and jxorfw='" + jxorfw
+					+ "' and locsite ='" + station + "'";
 
-				IJpoSet locations = MroServer.getMroServer().getSysJpoSet(
-						"LOCATIONS", where);
-				if (locations.count() > 0) {
-					String loc = locations.getJpo().getString("LOCATION");
-					getJpo().setValue("LOCATION", loc,
-							GWConstant.P_NOVALIDATION);
-				}
-
-			} else {
-				getJpo().getField("SQN").action();
+			IJpoSet locations = MroServer.getMroServer().getSysJpoSet("LOCATIONS", where);
+			if (locations.count() > 0) {
+				String loc = locations.getJpo().getString("LOCATION");
+				getJpo().setValue("LOCATION", loc, GWConstant.P_NOVALIDATION);
 			}
+
+		} else {
+			getJpo().getField("SQN").action();
 		}
 	}
 }
